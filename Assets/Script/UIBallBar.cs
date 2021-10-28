7502a26a5f98ddec81311f0727e0cdbd2a6d1e0c
ofgonen <ofgonen@gmail.com>
2021-10-28 12:31:18 +0300

    Implement Ball Height UI Bar

---
+++ Assets/Script/UIBallBar.cs
@@ -0,0 +1,65 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+
+public class UIBallBar : UILoadingBar
+{
+#region Fields
+    [ Header( "Event Listeners" ) ]
+    public EventListenerDelegateResponse levelStartedListener;
+    public EventListenerDelegateResponse ballCatchListener;
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+    protected override void OnEnable()
+    {
+		base.OnEnable();
+
+		levelStartedListener.OnEnable();
+		ballCatchListener.OnEnable();
+	}
+
+    protected override void OnDisable()
+    {
+		base.OnDisable();
+
+		levelStartedListener.OnDisable();
+		ballCatchListener.OnDisable();
+	}
+
+    protected override void Awake()
+    {
+		base.Awake();
+
+		levelStartedListener.response  = LevelStartedResponse;
+		ballCatchListener.response     = BallCatchResponse;
+	}
+
+#endregion
+
+#region API
+#endregion
+
+#region Implementation
+    private void LevelStartedResponse()
+    {
+		GoToTargetPosition();
+	}
+
+    private void BallCatchResponse() 
+    {
+		GoToStartPosition();
+    }
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
